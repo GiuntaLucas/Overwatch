75d3438dcf99f66d58c8ef17873d1dc09ffac1b1
GiuntaLucas <lucas-giunta@outlook.com>
2022-10-17 18:23:07 +0200

    astro ssr folder created

---
+++ astro-SSR/src/components/user-button.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { user } from "../stores/user.store";
+  const currentUser = user.value;
+
+  function handleClick() {
+    if($currentUser) {
+        user.set(null);
+    }
+    location.replace('/');
+  }
+</script>
+
+<button type="button" on:click={handleClick}>
+  {#if $currentUser}
+    Logout
+  {:else}
+    Login
+  {/if}
+</button>
---
+++ astro-SSR/src/models/capacity.ts
@@ -0,0 +1,12 @@
+export class Capacity{
+    name:string;
+    description:string;
+    video:string;
+    image:string;
+
+    display:boolean;
+
+    constructor(){
+        this.display = true;
+    }
+}
