a9d2dc4b44e66c91b05c6a68e7322517c90a0043
GiuntaLucas <lucas-giunta@outlook.com>
2022-10-18 10:52:47 +0200

    Remove astro ssr

--- astro-SSR/src/models/notification.ts
+++
@@ -1,4 +0,0 @@
-export interface Notification {
-    message: string;
-    severity: 'success' | 'warning' | 'error';
-}
--- astro-SSR/src/models/user.ts
+++
@@ -1,4 +0,0 @@
-export interface User{
-    battleTag:string;
-    password: string;
-}
